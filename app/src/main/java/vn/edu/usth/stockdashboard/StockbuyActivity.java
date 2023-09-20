caa7e1f39e3efdffdf0fe15ca8ea61a9804a1ed7
bhhoang <canhchimcuajin@gmail.com>
2023-09-21 03:23:55 +0700

    l

---
+++ app/src/main/java/vn/edu/usth/stockdashboard/StockbuyActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.usth.stockdashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StockbuyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stockbuy);
+    }
+}
