9bd9a3b38f68a91606a372caa9cd0ab78a8048ff
K0ntact <duyan5745@gmail.com>
2023-09-20 19:15:41 +0700

    fix: resolve merge conflict

--- app/src/main/java/vn/edu/usth/stockdashboard/MainActivity.java
+++ app/src/main/java/vn/edu/usth/stockdashboard/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
         CandleChartFragment firstFragment = new CandleChartFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.chartContainerView, firstFragment).commit();
 
-
         setContentView(R.layout.activity_main);
     }
 }
