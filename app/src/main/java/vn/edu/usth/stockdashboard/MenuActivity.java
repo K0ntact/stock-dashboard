563beb257d292e3df69aa565ca3a518ff1e15845
DucQuyBI12-376 <125538353+DucQuyBI12-376@users.noreply.github.com>
2023-09-21 09:46:54 +0700

    Add Menu Activity

--- app/src/main/java/vn/edu/usth/stockdashboard/MenuActivity.java
+++ app/src/main/java/vn/edu/usth/stockdashboard/MenuActivity.java
@@ -1,13 +1,14 @@
 package vn.edu.usth.stockdashboard;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class StockActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stock);
+        setContentView(R.layout.activity_menu);
     }
 }
