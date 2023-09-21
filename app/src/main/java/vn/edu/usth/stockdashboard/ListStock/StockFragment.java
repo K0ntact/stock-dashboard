141f3d8d78a9cf56409e803f4cb3f484758357c3
K0ntact <duyan5745@gmail.com>
2023-09-21 15:12:33 +0700

    fix: wrong layout file

--- app/src/main/java/vn/edu/usth/stockdashboard/ListStock/StockFragment.java
+++ app/src/main/java/vn/edu/usth/stockdashboard/ListStock/StockFragment.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import vn.edu.usth.stockdashboard.R;
@@ -14,7 +13,7 @@ public class StockFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_stock, container, false);
+        View view = inflater.inflate(R.layout.fragment_list_stock, container, false);
         return view;
     }
 }
