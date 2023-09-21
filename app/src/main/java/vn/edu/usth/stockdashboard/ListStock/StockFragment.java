4f3789a24eec021f64d94dd89c3a83883a9d38ea
K0ntact <duyan5745@gmail.com>
2023-09-21 12:55:24 +0700

    refractor: replace Stock Activity with Stock Fragment

---
+++ app/src/main/java/vn/edu/usth/stockdashboard/ListStock/StockFragment.java
@@ -0,0 +1,20 @@
+package vn.edu.usth.stockdashboard.ListStock;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import vn.edu.usth.stockdashboard.R;
+
+public class StockFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_stock, container, false);
+        return view;
+    }
+}
