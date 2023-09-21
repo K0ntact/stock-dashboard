d06558770af3a64a482948204b9e42f48243fd97
K0ntact <duyan5745@gmail.com>
2023-09-21 13:57:10 +0700

    feat: set logic for bottom navigation bar

--- app/src/main/java/vn/edu/usth/stockdashboard/MainActivity.java
+++ app/src/main/java/vn/edu/usth/stockdashboard/MainActivity.java
@@ -4,10 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import vn.edu.usth.stockdashboard.ListStock.StockFragment;
+
+public class MainActivity extends AppCompatActivity{
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if(id == R.id.listTab) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new StockFragment()).commit();
+                return true;
+            } else if (id == R.id.viewListTab) {
+                return true;
+            }
+            else return id == R.id.menuTab;
+        });
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new StockFragment()).commit();
     }
 }
