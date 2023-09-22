8fdb9417e20fe045f2b61085d19708d5ee4edfe4
K0ntact <duyan5745@gmail.com>
2023-09-22 21:27:32 +0700

    feat(MenuAfterLoginFragment): open detail view on item click

--- app/src/main/java/vn/edu/usth/stockdashboard/Menu/MenuAfterLoginFragment.java
+++ app/src/main/java/vn/edu/usth/stockdashboard/Menu/MenuAfterLoginFragment.java
@@ -1,5 +1,6 @@
 package vn.edu.usth.stockdashboard.Menu;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,15 +12,16 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
+import vn.edu.usth.stockdashboard.DetailStock.StockDetailActivity;
 import vn.edu.usth.stockdashboard.R;
 import vn.edu.usth.stockdashboard.ListStock.StockItem;
 import vn.edu.usth.stockdashboard.ListStock.StockListAdapter;
 
 public class MenuAfterLoginFragment extends Fragment {
-    private ArrayList<StockItem> menuEntries = new ArrayList<>();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ArrayList<StockItem> menuEntries = new ArrayList<>();
         menuEntries.add(new StockItem("Test", "It's just a Text", "99,69 US$", "+0,19%")); // Sample data point 1
         menuEntries.add(new StockItem("TEST1", "It's still just a Text", "11.222,33", "+0,22%")); // Sample data point 2
         menuEntries.add(new StockItem("TEST2", "Just a Text.", "56,65 US$", "+0,35%")); // Sample data point 3
@@ -29,6 +31,15 @@ public class MenuAfterLoginFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_menu_after_login, container, false);
         ListView listView = view.findViewById(R.id.listMenuView);
         // Set the item click listener for the ListView
+        listView.setOnItemClickListener(
+                (adapterView, view1, i, l) -> {
+                    Intent intent = new Intent(getActivity(), StockDetailActivity.class);
+                    intent.putExtra("stockName", menuEntries.get(i).getSymbol());
+                    intent.putExtra("companyName", menuEntries.get(i).getName());
+                    intent.putExtra("money", menuEntries.get(i).getMoney());
+                    intent.putExtra("percentage", menuEntries.get(i).getPercentage());
+                    startActivity(intent);
+                });
         StockListAdapter adapter = new StockListAdapter(requireContext(), menuEntries);
         listView.setAdapter(adapter);
         return view;
