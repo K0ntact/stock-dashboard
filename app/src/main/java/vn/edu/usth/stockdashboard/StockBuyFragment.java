b0dd212ff655cbd90495db3ad234b5c3951c0bdd
K0ntact <duyan5745@gmail.com>
2023-09-22 16:43:25 +0700

    refactor: change name from activity to fragment

--- app/src/main/java/vn/edu/usth/stockdashboard/StockBuyFragment.java
+++ app/src/main/java/vn/edu/usth/stockdashboard/StockBuyFragment.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.core.text.HtmlCompat;
 import androidx.fragment.app.Fragment;
 
-import android.content.DialogInterface;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -21,7 +20,7 @@ public class StockBuyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         AtomicInteger tradeState = new AtomicInteger(); // 0 mean buy, 1 mean sell
-        View view = inflater.inflate(R.layout.activity_stockbuy, container, false);
+        View view = inflater.inflate(R.layout.fragment_stockbuy, container, false);
         Button buybtn = (Button) view.findViewById(R.id.buybtn);
         Button sellbtn = (Button) view.findViewById(R.id.sellbtn);
         Button placeorderbtn = (Button) view.findViewById(R.id.placeorder);
@@ -53,17 +52,14 @@ public class StockBuyFragment extends Fragment {
             builder.setTitle(HtmlCompat.fromHtml("<font color='#FFFFFF'>Order Confirm</font>", HtmlCompat.FROM_HTML_MODE_LEGACY));
             String tradeType = tradeState.get() == 0 ? "buy" : "sell";
             builder.setMessage(HtmlCompat.fromHtml("<font color='#FFFFFF'>Are you sure you want to " + tradeType + " this stock</font>", HtmlCompat.FROM_HTML_MODE_LEGACY));
-            builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    AlertDialog alertDialog = new AlertDialog.Builder(StockBuyFragment.this.requireContext()).create();
-                    alertDialog.setTitle(HtmlCompat.fromHtml("<font color='#FFFFFF'>Order Placed</font>", HtmlCompat.FROM_HTML_MODE_LEGACY));
-                    alertDialog.setMessage(HtmlCompat.fromHtml("<font color='#FFFFFF'>Your order has been placed</font>", HtmlCompat.FROM_HTML_MODE_LEGACY));
-                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                            (dialog, which) -> dialog.dismiss());
-                    Objects.requireNonNull(alertDialog.getWindow()).setBackgroundDrawableResource(android.R.color.background_dark);
-                    alertDialog.show();
-                }
+            builder.setPositiveButton("Confirm", (dialogInterface, i) -> {
+                AlertDialog alertDialog = new AlertDialog.Builder(StockBuyFragment.this.requireContext()).create();
+                alertDialog.setTitle(HtmlCompat.fromHtml("<font color='#FFFFFF'>Order Placed</font>", HtmlCompat.FROM_HTML_MODE_LEGACY));
+                alertDialog.setMessage(HtmlCompat.fromHtml("<font color='#FFFFFF'>Your order has been placed</font>", HtmlCompat.FROM_HTML_MODE_LEGACY));
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        (dialog, which) -> dialog.dismiss());
+                Objects.requireNonNull(alertDialog.getWindow()).setBackgroundDrawableResource(android.R.color.background_dark);
+                alertDialog.show();
             });
             builder.setNegativeButton("Cancel", (dialogInterface, i) -> dialogInterface.dismiss());
             builder.setCancelable(true);
