8cef29a1c92d913ca0410cd2e65ef44507d97fa0
bhhoang <canhchimcuajin@gmail.com>
2023-09-23 15:02:57 +0700

    Optimize overdraw and backstack clear

--- app/src/main/java/vn/edu/usth/stockdashboard/Menu/MenuFragment.java
+++ app/src/main/java/vn/edu/usth/stockdashboard/Menu/MenuFragment.java
@@ -1,13 +1,18 @@
 package vn.edu.usth.stockdashboard.Menu;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.text.HtmlCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +22,7 @@ import vn.edu.usth.stockdashboard.R;
 
 public class MenuFragment extends Fragment {
     private boolean isLogin = false;
+    @SuppressLint("SetTextI18n")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_menu, container, false);
@@ -24,14 +30,39 @@ public class MenuFragment extends Fragment {
         if (savedInstanceState != null)
             isLogin = savedInstanceState.getBoolean("isLogin");
         if (isLogin) {
-            requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.menuFragmentContainer, new MenuAfterLoginFragment()).commit();
+                FragmentTransaction transaction = requireActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.menuFragmentContainer, new MenuAfterLoginFragment());
+                requireActivity().getSupportFragmentManager().popBackStack();
+            transaction.commit();
         }
 
         TextView logout = view.findViewById(R.id.logoutView);
         logout.setOnClickListener(view1 -> {
-            Intent intent = new Intent(MenuFragment.this.getActivity(), MainActivity.class);
-            intent.putExtra("isLogin", false);
-            startActivity(intent);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(MenuFragment.this.requireContext());
+
+            View s = getLayoutInflater().inflate(R.layout.dialog_style, null);
+            TextView title = s.findViewById(R.id.titleAlert);
+            title.setText("Logout");
+            TextView content = s.findViewById(R.id.confirmMessage);
+            content.setText("Are you sure to logout?");
+            Button confirm = s.findViewById(R.id.cfbtn);
+            confirm.setText(HtmlCompat.fromHtml("<b>Logout</b>", HtmlCompat.FROM_HTML_MODE_COMPACT));
+            Button cancel = s.findViewById(R.id.cancelbtn);
+            cancel.setText("Skip");
+            builder.setView(s);
+            AlertDialog d = builder.create();
+            d.show();
+             confirm.setOnClickListener(v -> {
+                 d.dismiss();
+                 Intent intent = new Intent(MenuFragment.this.getActivity(), MainActivity.class);
+                 intent.putExtra("isLogin", false);
+                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                 startActivity(intent);
+                 Toast.makeText(MenuFragment.this.requireContext(), "Logged out!", Toast.LENGTH_SHORT).show();
+             });
+             cancel.setOnClickListener(v -> d.dismiss());
         });
 
         //Add the Refresh Button
@@ -44,14 +75,20 @@ public class MenuFragment extends Fragment {
 //                R.anim.zoom_in_enter,
 //                R.anim.zoom_in_exit);
             Intent intent = new Intent(MenuFragment.this.getActivity(), MenuSearchActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
         });
         ImageView actionNotification = view.findViewById(R.id.actionNotification);
         actionNotification.setOnClickListener(view1 -> {
             ActivityOptionsCompat options = ActivityOptionsCompat.makeCustomAnimation(MenuFragment.this.requireActivity(), R.anim.zoom_in_enter, R.anim.zoom_in_exit);
             Intent intent = new Intent(MenuFragment.this.getActivity(), NotiMenuActivity.class);
+            // Clear backstack
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent, options.toBundle());
         });
+
         return view;
     }
 }
