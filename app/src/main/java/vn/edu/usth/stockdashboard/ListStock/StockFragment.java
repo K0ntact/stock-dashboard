2f9b23f9f0f20ab189c7abbd2a052c35a1cb8479
K0ntact <duyan5745@gmail.com>
2023-09-23 09:33:15 +0700

    refactor: add more stock symbols

--- app/src/main/java/vn/edu/usth/stockdashboard/ListStock/StockFragment.java
+++ app/src/main/java/vn/edu/usth/stockdashboard/ListStock/StockFragment.java
@@ -22,11 +22,20 @@ public class StockFragment extends Fragment {
         ListView listView = view.findViewById(R.id.listView);
 
         ArrayList<StockItem> entries = new ArrayList<>();
-        entries.add(new StockItem("VNM", "VanEck VietNam ETF", "15,56 US$", "0,19%")); // Sample data point 1
-        entries.add(new StockItem("Dow Jones", "Dow Jones Industrial Average", "34.576,59", "0,22%")); // Sample data point 2
+        entries.add(new StockItem("VNM", "VanEck VietNam ETF", "15,56 US$", "0,19%"));
         entries.add(new StockItem("AAPL", "Apple Inc.", "178,18 US$", "0,35%")); // Sample data point 3
         entries.add(new StockItem("SBUX", "Starbucks Corporation", "95,28 US$","0,19%"));
-        entries.add(new StockItem("NKE", "NIKE, Inc.", "97,67 US$", " 0,27%"));
+        entries.add(new StockItem("NKE", "NIKE, Inc.", "97,67 US$", "0,27%"));
+        entries.add(new StockItem("TSLA", "Tesla, Inc.", "709,44 US$", "0,19%"));
+        entries.add(new StockItem("AMZN", "Amazon.com, Inc.", "3.372,20 US$", "0,27%"));
+        entries.add(new StockItem("FB", "Facebook, Inc.", "331,26 US$", "0,19%"));
+        entries.add(new StockItem("GOOGL", "Alphabet Inc.", "2.431,38 US$", "0,27%"));
+        entries.add(new StockItem("MSFT", "Microsoft Corporation", "259,43 US$", "0,19%"));
+        entries.add(new StockItem("NVDA", "NVIDIA Corporation", "191,05 US$", "0,27%"));
+        entries.add(new StockItem("PYPL", "PayPal Holdings, Inc.", "279,50 US$", "0,19%"));
+        entries.add(new StockItem("TSM", "Taiwan Semiconductor Manufacturing Company Limited", "117,00 US$", "0,27%"));
+        entries.add(new StockItem("V", "Visa Inc.", "226,00 US$", "0,19%"));
+        entries.add(new StockItem("WMT", "Walmart Inc.", "142,00 US$", "0,27%"));
         // Set the item click listener for the ListView
         listView.setOnItemClickListener(
                 (adapterView, view1, i, l) -> {
