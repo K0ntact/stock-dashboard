2d2547c29e4b82c7fc5e4d70d7c81c0f0bf9c8b1
K0ntact <duyan5745@gmail.com>
2023-10-06 15:52:58 +0700

    feat: UI update

--- application/app/src/main/java/vn/edu/usth/stockdashboard/DetailStock/ChartAdapter.java
+++ application/app/src/main/java/vn/edu/usth/stockdashboard/DetailStock/ChartAdapter.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 public class ChartAdapter extends FragmentStateAdapter {
+    private String symbol;
     public ChartAdapter(StockDetailActivity activity) {
         super(activity);
     }
@@ -18,13 +19,21 @@ public class ChartAdapter extends FragmentStateAdapter {
     @Override
     public Fragment createFragment(int position) {
         switch (position) {
-            case 0: return new CandleChartFragment("6M");
-            case 1: return new CandleChartFragment("1D");
-            case 2: return new CandleChartFragment("2D");
-            case 3: return new CandleChartFragment("5D");
-            case 4: return new CandleChartFragment("ALL");
+            case 0: return new CandleChartFragment("1D", symbol);
+            case 1: return new CandleChartFragment("2D", symbol);
+            case 2: return new CandleChartFragment("5D", symbol);
+            case 3: return new CandleChartFragment("6M", symbol);
+            case 4: return new CandleChartFragment("ALL", symbol);
         }
-        return new CandleChartFragment("6M");
+        return new CandleChartFragment("1D", symbol);
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
     }
 }
 
